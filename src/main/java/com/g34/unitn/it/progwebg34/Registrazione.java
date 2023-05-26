7b5aceb2f0df98b0d17d16343d127443dc3ae63f
Luca Dematté <luca.dematte2020@gmail.com>
2023-05-26 15:38:48 +0200

    Inserimento utenti funzionante

---
+++ src/main/java/com/g34/unitn/it/progwebg34/Registrazione.java
@@ -0,0 +1,72 @@
+package com.g34.unitn.it.progwebg34;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.*;
+
+
+@WebServlet(name = "Registrazione", value = "/registrazione")
+public class Registrazione extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/registrazione.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        //prelievo dei parametri dal body della richiesta
+        String nome = request.getParameter("nome");
+        String cognome = request.getParameter("cognome");
+        String dataNascita = request.getParameter("dataNascita");
+        String email = request.getParameter("email");
+        String telefono = request.getParameter("telefono");
+        String tmp = request.getParameter("tipoRegistrazione");
+        int tipoRegistrazione = Integer.parseInt(request.getParameter("tipoRegistrazione"));
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String ripetiPassword = request.getParameter("ripetiPassword");
+
+
+        //eseguire controlli sui dati anche lato server?
+
+        //COLLEGAMENTO AL DATABASE
+        String url = "jdbc:derby://localhost:1527/Tum4WorldDB34";
+
+        try {
+            //caricamento driver
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+            //apertura connessione al database
+            Connection con = DriverManager.getConnection(url);
+
+            //preparazione statement
+            String query = "INSERT INTO Iscritto (Username,Password,Nome,Cognome,DataNascita,Email,Telefono,Tipologia) VALUES (?,?,?,?,?,?,?,?)";
+
+            PreparedStatement s = con.prepareStatement(query);
+            s.setString(1, username);
+            s.setString(2, password);
+            s.setString(3, nome);
+            s.setString(4, cognome);
+            s.setString(5, dataNascita);
+            s.setString(6, email);
+            s.setString(7, telefono);
+            s.setInt(8, tipoRegistrazione);
+
+            //invio statement
+            s.executeUpdate();
+
+            //chiusura connessione
+            con.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+}
--- src/main/webapp/WEB-INF/registrazione.jsp
+++ src/main/webapp/WEB-INF/registrazione.jsp
@@ -6,7 +6,7 @@
     <body>
     <fieldset>
         <legend>Registrati alla nostra associazione!</legend>
-        <form action="">
+        <form action="<%= response.encodeURL("registrazione")%>" method="post">
             <label for="nome">Nome: </label><input type="text" id="nome" name="nome" required><br>
             <label for="cognome">Cognome: </label><input type="text" id="cognome" name="cognome" required><br>
             <label for="data-nascita">Data di nascita: </label><input type="date" id="data-nascita" name="dataNascita" required><br>
@@ -14,13 +14,14 @@
             <label for="telefono">Telefono: </label><input type="tel" id="telefono" name="telefono"><br>
 
             Modalità di registrazione:<br>
-            <label><input type="radio" name="tipoRegistrazione" value="simpatizzante" required> Simpatizzante</label><br>
-            <label><input type="radio" name="tipoRegistrazione" value="aderente" required> Aderente</label><br>
+            <input type="radio" id="radioSimpatizzante" name="tipoRegistrazione" value="0" required><label for="radioSimpatizzante"> Simpatizzante</label><br>
+            <input type="radio" id="radioAderente" name="tipoRegistrazione" value="1" required><label for="radioAderente"> Aderente</label><br>
 
             <label for="username">Username: </label><input type="text" id="username" name="username" required><br>
-            <label for="password">Password: </label><input type="password" id="password" name="password" minlength="8" pattern="" required><br>
-            <label for="ripeti-password">Ripeti la password: </label><input type="password" id="ripeti-password" name="ripetiPassword" minlength="8" pattern="" required><br>
+            <label for="password">Password: </label><input type="password" id="password" name="password" minlength="8" required><br>
+            <label for="ripeti-password">Ripeti la password: </label><input type="password" id="ripeti-password" name="ripetiPassword" minlength="8" required><br>
 
+            <input type="submit" value="Invia input">
             <button type="submit">Invia</button>
             <button type="reset">Reset</button>
         </form>
